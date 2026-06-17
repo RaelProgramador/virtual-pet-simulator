d324a567c11fd5a6f6c92c4e4375b82d3de57d8f
Israel de Castro <89636714+RaelProgramador@users.noreply.github.com>
2026-06-17 07:35:15 +0100

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,1 +1,37 @@
-318YO
+public class Cat extends Pet {
+    private String furLength; 
+
+    public Cat(String name, String furLength) {
+        super(name);
+        this.furLength = furLength;
+    }
+
+    public String getFurLength() {
+        return furLength;
+    }
+
+    public void setFurLength(String furLength) {
+        this.furLength = furLength;
+    }
+
+    @Override
+    public void feed() {
+        setHungry(getHungry() - 3);
+        setEnergy(getEnergy() + 1);
+        setHappiness(getHappiness() + 1);
+        System.out.println(getName() + " is eating fish. Yum!");
+    }
+
+    @Override
+    public void play() {
+        setHappiness(getHappiness() + 3);
+        setEnergy(getEnergy() - 2);
+        setHungry(getHungry() + 1);
+        System.out.println(getName() + " is playing with a ball of yarn!");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " says: Meow!");
+    }
+}
